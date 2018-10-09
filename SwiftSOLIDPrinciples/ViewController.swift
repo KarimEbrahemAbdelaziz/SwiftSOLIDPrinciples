a798f8b9c830769ed83fd84179052c694cec0bb6
Karim Ebrahem <karimabdelazizmansour@gmail.com>
2018-10-09 14:55:12 +0200

    Initial commit

---
+++ SwiftSOLIDPrinciples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSOLIDPrinciples
+//
+//  Created by Karim Ebrahem on 10/9/18.
+//  Copyright © 2018 KarimEbrahem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
